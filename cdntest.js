fea67bd65ffdff02df103a3a495a41828951982d
Anton Gladyshev <returnstrike@yandex.ru>
2018-02-12 13:23:02 +0600

    create testcdn.js

---
+++ cdntest.js
@@ -0,0 +1,7 @@
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+<script>
+   window.jQuery || document.write('<script src="/path/to/your/jquery"><\/script>');
+</script>
+</head>
+<!--'<'+'/script>'-->
